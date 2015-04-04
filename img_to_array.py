fb55bbf257318534caa0df44a49cd5cc3f25ffe3
andrey.gvrd@gmail.com <andrey.gvrd@gmail.com>
2015-04-04 21:23:41 +0400

    major rewrite, still very much wip

--- img_to_array.py
+++ img_to_array.py
@@ -1,13 +1,107 @@
 from __future__ import print_function
 from PIL import Image
 import math
-import sys #Command Line arguments
-import colorsys
+import sys 		# Command Line arguments
+import colorsys	# HSL <-> RGB conversion
+import copy
 
-def is_white(r, g, b):
-	return (r == 255 and g == 255 and b == 255)
+# Algorithm:
+# 1. Make sure supplied image has:
+# 	1.1. Black/white background
+# 	1.2. Line (any object really) of a single color <- not necessary?
+# 2. Get the color of the line in RGB
+# 3. Get an array of brightness values from line's y (height) value - [0 .. 1]
+# 4. Correct the brightness array with log function to account for how eye perceives brightness. See more at:
+#    http://what-when-how.com/display-interfaces/the-human-visual-system-display-interfaces-part-2
+# 5. Based on extracted line's color and image's height create a pallet in HLS
+# 6. Create an array of RGB colors based on the pallet and corrected brightness array
+
+# Notes:
+# - Top-left pixel must be a part of the background
+
+class Color:
+
+	def __init__(self, r, g, b):
+		self.r = r
+		self.g = g
+		self.b = b
+
+	def __eq__(self, other):
+		return (self.r == other.r and
+				self.g == other.g and
+				self.b == other.b)
+
+	def is_white(self):
+		return (self.r == 255 and 
+				self.g == 255 and 
+				self.b == 255)
+
+	def is_black(self):
+		return (self.r == 0 and 
+				self.g == 0 and 
+				self.b == 0)
 
 class img_to_array:
+
+	def __init__(self, im_rgb):
+		self.im_rgb = im_rgb
+		self.width, self.height = im_rgb.size
+
+	line_color = Color(0, 0, 0)
+	bg_color   = Color(0, 0, 0)
+	hls_palette = {}
+
+	# Base colors for hls_palette. Would have used Color class, but it works with integers only for now.
+	base_h = 0.0
+	base_l = 0.0
+	base_s = 0.0
+
+	def image_check(self):
+		bg_found = False
+		line_found = False
+		for hor in range(self.width):
+			for ver in range(self.height):
+				current_pixel = Color(*self.im_rgb.getpixel((hor, ver)))
+				if (current_pixel.is_white() or current_pixel.is_black()):
+					if (bg_found == False):
+						bg_found = True
+						self.bg_color = copy.copy(current_pixel)
+				else:
+					if (line_found == False):
+						line_found = True
+						self.line_color = copy.copy(current_pixel)
+		return (bg_found, line_found)
+
+	# Line in an image -> brightness [1.0] as distance from the bottom of the image
+	def brightness_extract(self):
+		brightness_array = []
+		for hor in range(self.width):
+			for ver in range(self.height):
+				current_pixel = Color(*self.im_rgb.getpixel((hor, ver)))
+				if (current_pixel == self.line_color):
+					brightness_array.append((self.height - ver) / self.height)
+					break
+		return brightness_array
+
+	# Creates a static table. Not very good when your indexes are floats.
+	def hls_palette_create(self):
+		self.base_h, self.base_l, self.base_s = colorsys.hls_to_rgb(self.line_color.r / 255.0, 
+			                                                        self.line_color.g / 255.0, 
+			                                                        self.line_color.b / 255.0)
+		l_step = self.base_l / self.height
+		l = self.base_l
+		for x in range(0, self.height):
+			self.hls_palette = {x / self.height: (self.h, l, self.s)} # Index is [0 - 1.0]
+			l -= l_step
+
+	# Reaturs an (h, l, s) value for any float value on the input. Basically [0.0 - 1.0] -> [0.0 - base_l]
+	def hls_palette_get(self):
+		return h, l, s = 
+
+	def create_color_array(self, corrected_brightness_array):
+		for x in range(self.width * 3):
+			h, l, s = self.hls_palette[corrected_brightness_array[x]]
+
 	def c_array(self, full_path, array):
 		declaration_start = 'const uint8_t pattern[] = {\n'
 		declaration_end   = '};\n'
@@ -20,77 +114,39 @@ class img_to_array:
 			c_arr.write('\n')
 		c_arr.write(declaration_end)
 
-	# Line in an image -> distance from the bottom of the image
-	def array_extract(self, im_rgb, line_r, line_g, line_b):
-		width, height = im_rgb.size
-		array = []
-		for x in range (width):
-			for y in range (height):
-				r, g, b = im_rgb.getpixel((x,y))
-				if (line_r == r and line_g == g and line_b == b):
-					array.append(256 - y)
-					break
-		return array
-
-	# Values which depend on image size -> percentage [0 - 100]
-	def scale_8bit_to_percentage(self, array, height):
-		for x in range (len(array)):
-			array[x] = array[x] / (height / 100)
-		return array
-
-	# Scale brigthness percentage [0 - 100] logarithmically into [0 - 255]
-	def scale_percentage_to_log(self, array):
-		a = 0.977584631663603   # I found these terms on the internet and have no idea about
-		b = 0.055498961535023   # the theory behind them
-		for x in range (len(array)):
-			array[x] = math.floor(a * math.exp((b * array[x])) - 1)
-		return array
-
-	# Make sure there's background and a signle line of color
-	def im_prepare(self, im_rgb):
-		width, height = im_rgb.size
-		bg_found = False
-		line_found = False
-		told_about_colors = False
-		line_r = 0
-		line_g = 0
-		line_b = 0
-		for x in range (width):
-			for y in range (height):
-				r, g, b = im_rgb.getpixel((x,y))
-				if (is_white(r, g, b)):
-					if (bg_found == False):
-						print('BG found')
-						bg_found = True
-				else:
-					if (line_found == False):
-						line_r = r
-						line_g = g
-						line_b = b
-						print('Found line of color (%d, %d, %d)' % (line_r, line_g, line_b))
-						line_found = True
-					else:
-						if (r != line_r or g != line_g or b != line_b):
-							if (told_about_colors == False):
-								print('Found another line of color (%d, %d, %d)' % (r, g, b))
-								told_about_colors = True
-		return (line_r, line_g, line_b)
+def correct_brightness_log(array):
+	scale = 0.58   # To scale the logarithmic function to [0 - 1]
+	for x in range(len(array)):
+		array[x] = scale * (math.exp(array[x]) - 1)
+	return array
 
 def main():
-	i2a = img_to_array()
 
+	# Directories:
 	input_img_dir = 'input/'
 	debug_data_dir = 'debug/'
 	output_dir = 'output/'
 
-	print ('Number of arguments:', len(sys.argv))
-	print ('Argument list:', str(sys.argv))
+	# Open the image:
+	im_rgb = Image.open(input_img_dir + 'in.png').convert('RGB')
+	i2a = img_to_array(im_rgb)
 
-	# Oopen the image:
-	im = Image.open(input_img_dir + 'in.png')
-	im_rgb = im.convert('RGB')
-	(im_width, im_height) = im.size
+	# 1. and 2. (oops)
+	bg_found, line_found = i2a.image_check()
 
+	# 3.
+	brightness_array = i2a.brightness_extract()
+
+	# 4.
+	corrected_brightness_array = correct_brightness_log(brightness_array)
+
+	# 5.
+	i2a.hls_palette_create()
+
+	# 6.
+	color_array = i2a.create_color_array(corrected_brightness_array)
+
+	'''
 	# Get base color line:
 	(line_r, line_g, line_b) = i2a.im_prepare(im_rgb)
 	(line_h, line_l, line_s) = colorsys.rgb_to_hls(line_r/255.0, line_g/255.0, line_b/255.0)
@@ -163,6 +219,7 @@ def main():
 			out_array.write(str(array[x]) + '\t')
 
 	c_arr = i2a.c_array(output_dir + 'c_array.h', array)
+	'''
 
 if __name__ == '__main__':
 	main()
